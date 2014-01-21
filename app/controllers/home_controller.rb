7a277b8ec1fd8c91cdd36cdeb28cc97968666665
Jerry Tao <taojay315@gmial.com>
2014-01-21 05:00:57 -0500

    Fix textarea \n error.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   def index
   end
   def create
-    @weibo = Weibo.create(weibo:params[:content])
+    @weibo = Weibo.create(weibo:params[:content].gsub("\n","<br>"))
     render :text=>"#{@weibo.image.url}"
   end
 end
