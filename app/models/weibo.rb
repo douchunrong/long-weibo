bcd28a9c434045a102f20376368a3b946241b34d
Jerry Tao <taojay315@gmial.com>
2014-01-21 05:05:52 -0500

    Fix height limit.

--- app/models/weibo.rb
+++ app/models/weibo.rb
@@ -5,7 +5,7 @@ class Weibo < ActiveRecord::Base
   def generate_image
       file = Tempfile.new(["template_#{self.id.to_s}", 'jpg'], 'tmp', :encoding => 'ascii-8bit')
       weibo = "<!DOCTYPE html><html lang='zh-cn'><head><meta charset='UTF-8'></head><body>#{self.weibo}</body></html>"
-      file.write(IMGKit.new(weibo, quality: 50, width: 600).to_jpg)
+      file.write(IMGKit.new(weibo, quality: 50, width: 600,height:0).to_jpg)
       file.flush
       self.image = file
       self.save
