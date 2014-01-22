a959bbf9aad185f21a8cd02ebd18eebd559b19d9
Jerry Tao <taojay315@gmial.com>
2014-01-21 22:05:11 -0500

    Move gsub to model

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   def index
   end
   def create
-    @weibo = Weibo.create(weibo:params[:content].gsub("\n","<br>"))
+    @weibo = Weibo.create(weibo:params[:content])
     render :text=>"#{@weibo.image.url}"
   end
 end
--- app/models/weibo.rb
+++ app/models/weibo.rb
@@ -4,7 +4,7 @@ class Weibo < ActiveRecord::Base
   private 
   def generate_image
       file = Tempfile.new(["template_#{self.id.to_s}", 'jpg'], 'tmp', :encoding => 'ascii-8bit')
-      weibo = "<!DOCTYPE html><html lang='zh-cn'><head><meta charset='UTF-8'></head><body>#{self.weibo}</body></html>"
+      weibo = "<!DOCTYPE html><html lang='zh-cn'><head><meta charset='UTF-8'></head><body>#{self.weibo.gsub("\n","<br />")}</body></html>"
       file.write(IMGKit.new(weibo, quality: 50, width: 600,height:0).to_jpg)
       file.flush
       self.image = file
